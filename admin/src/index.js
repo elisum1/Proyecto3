cd1a5fc6740a98e7a989597cff6aaefe4e7c04a9
Elias Urrieta <elias.um94@gmail.com>
2024-08-13 10:50:41 -0500

    Despliegue

--- admin/src/index.js
+++ admin/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { DarkModeContextProvider } from "./context/darkModeContext";
-import './index.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
